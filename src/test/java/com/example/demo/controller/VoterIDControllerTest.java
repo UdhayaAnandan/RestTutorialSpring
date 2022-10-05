61f673b94116e7a1e48c101d3997b446d4396aa1
udhay <contactudhayaanandan@gamil.com>
2022-10-05 23:53:38 +0530

    UnitTest

---
+++ src/test/java/com/example/demo/controller/VoterIDControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VoterIDControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
